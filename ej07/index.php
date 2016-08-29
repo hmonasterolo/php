068898bd44a5ed0999b5b8e080c36a4d0cb56e7b
Luciano Ropero <lropero@gmail.com>
2016-08-29 14:24:03 -0300

    Ejercicio 7

---
+++ ej07/index.php
@@ -0,0 +1,20 @@
+<?php
+function area($tipo, $objeto) {
+	// DEFINIR
+}
+
+$rectangulo = [
+	$x => 11,
+	$y => 7
+];
+
+$circulo = [
+	$r => 4;
+];
+
+$areaRectangulo = area('rectangulo', $rectangulo);
+$areaCirculo = area('circulo', $circulo);
+
+var_dump($areaRectangulo);
+var_dump($areaCirculo);
+?>
