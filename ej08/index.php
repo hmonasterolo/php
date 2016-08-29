cb3a81b0c0bac0123fcace94fad8179e6dd04f8d
Luciano Ropero <lropero@gmail.com>
2016-08-29 16:31:07 -0300

    Ejercicio 8

---
+++ ej08/index.php
@@ -0,0 +1,18 @@
+<?php
+include('suma.php');
+
+$arrayDeNumeros = [9, 5, 41, 11, 28, 47, 31, 49, 43, 27];
+imprimirSumaIncremental($arrayDeNumeros);
+
+/*
+Debe imprimir:
+9
+14
+55
+66
+94
+...
+
+Acordate que para mostrar un salto de línea en el navegador se debe imprimir <br />, por ejemplo echo('hola<br />');
+*/
+?>
