3f4cf281a693c5d00dd70804c1e35abb24223827
Luciano Ropero <lropero@gmail.com>
2016-08-31 12:32:52 -0300

    Arreglo menor al ejercicio 10

--- ej10/index.php
+++ ej10/index.php
@@ -2,24 +2,24 @@
 include('calculadora.php');
 
 // Memoria 0
-$resultado1 = sumar(3, 4); // 7
-guardarEnMemoria($resultado1);
+$resultado = sumar(3, 4); // 7
+guardarEnMemoria($resultado);
 
 // Memoria 1
-$resultado2 = multiplicar(7, 6); // 42
-guardarEnMemoria($resultado2, 1);
+$resultado = multiplicar(7, 6); // 42
+guardarEnMemoria($resultado, 1);
 
 // Memoria 2
-$resultado3 = restar(5, 3); // 2
-guardarEnMemoria($resultado3, 2);
+$resultado = restar(5, 3); // 2
+guardarEnMemoria($resultado, 2);
 
 // Memoria 3
-$resultado4 = sumar(5, 4); // 9
-guardarEnMemoria($resultado4, 3);
+$resultado = sumar(5, 4); // 9
+guardarEnMemoria($resultado, 3);
 
 // Memoria 4
-$resultado5 = dividir(15, 3); // 5
-guardarEnMemoria($resultado5, 4);
+$resultado = dividir(15, 3); // 5
+guardarEnMemoria($resultado, 4);
 
 $cuenta = sumar(dividir(restar(obtenerMemoria(1), obtenerMemoria(2)), obtenerMemoria(4)), restar(obtenerMemoria(3), obtenerMemoria(0)));
 var_dump($cuenta); // 10
